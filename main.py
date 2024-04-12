cff00be5b7b700cbfae4ffb123f7deb5ce4cdbc4
PaoloP84 <double.pi@hotmail.it>
2024-04-12 16:52:59 +0200

    Added the possibility of launching the GUI in full-screen mode (i.e., the GUI size matches the screen size). The GUI can be closed by pressing the ESC button of the keyboard.

--- main.py
+++ main.py
@@ -2,22 +2,35 @@
 # @author Paolo Pagliuca <paolo.pagliuca@istc.cnr.it>
 
 import os, sys, getopt, importlib
+import argparse
 from gui import GUIFactory
 ########################################################################################
 ## main functions
 ########################################################################################
 
-def start(argv):
+def start(argv, fullscreen=None):
     
     global tk
     import tkinter as tk
     root = tk.Tk()
     root.title('PhyloTreeGUIv1.0')
     root.lift()
+    if fullscreen:
+        width, height = root.winfo_screenwidth(), root.winfo_screenheight()
+        root.geometry('%dx%d+0+0' % (width, height))
+    
+    # Window can be closed by pressing "ESC" keyboard button
+    def close_escape(event=None):
+        root.destroy()
+    
+    root.bind("<Escape>", close_escape)
     GUIFactory.create_gui(root)
     root.mainloop()
 
 if __name__ == "__main__":
-    start(sys.argv[1:])
+    parser = argparse.ArgumentParser(description='PhyloTreeGUIv1.0')
+    parser.add_argument('--fullscreen', action=argparse.BooleanOptionalAction)
+    arg = parser.parse_args()
+    start(sys.argv[1:], fullscreen=arg.fullscreen)
 
 #sys.argv is the list of commandline arguments passed to the Python program
